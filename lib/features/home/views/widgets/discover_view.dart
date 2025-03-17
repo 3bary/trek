61c5dc7a5198b257b31d5498b8a194617a1327c8
Ahlam <129538648+Ahlamgomaa@users.noreply.github.com>
2025-03-17 02:13:30 +0200

    create Discover view

---
+++ lib/features/home/views/widgets/discover_view.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+import '../../../../core/utils/constants.dart';
+
+class DiscoverView extends StatelessWidget{
+  const DiscoverView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 20,
+      itemBuilder: (context, index) {
+        return  Container(
+            padding: EdgeInsets.all(10),
+        height: 150,
+        width: double.infinity,
+        margin: EdgeInsets.symmetric(vertical: 8, horizontal: 20),
+        decoration: BoxDecoration(
+        color: kTertiaryColor,
+        borderRadius: BorderRadius.circular(10),
+        )
+        );
+      }
+
+    );
+  }
+}
