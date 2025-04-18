e3c76778821bc7b592e9c35fe7aeb7b5a3c66a2a
Ahlam <129538648+Ahlamgomaa@users.noreply.github.com>
2025-04-19 00:14:26 +0200

    refact code

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
-
+import 'package:greendo/features/home/presentation/views/favorite_view.dart';
 
 import '../../../../core/utils/app_router.dart';
 
@@ -9,7 +9,6 @@ import '../../../profile/presentation/views/profile_view.dart';
 import '../view_model/navigation_cubit/navigation_cubit.dart';
 import 'discover_view.dart';
 
-import 'favorite_view.dart';
 import 'group_view.dart';
 import 'widgets/bottom_naviagation_bar.dart';
 
