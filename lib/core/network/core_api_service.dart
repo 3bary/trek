a799a81a68f3cdfbfd4876609ecac87d28a6536f
Mohamed Ashraf <medoalex350@gmail.com>
2025-04-29 19:40:14 +0300

    Implement IApiService for CoreApiService and add post method

--- lib/core/network/core_api_service.dart
+++ lib/core/network/core_api_service.dart
@@ -1,13 +1,22 @@
 import 'package:dio/dio.dart';
+import 'package:greendo/core/network/api_service.dart';
 
-class CoreApiService {
+class CoreApiService implements IApiService {
   final _baseUrl = 'https://final-project-production-5d66.up.railway.app/';
   final Dio _dio;
 
   CoreApiService(this._dio);
 
+  @override
   Future<Map<String, dynamic>> get({required String endpoint}) async {
     var response = await _dio.get('$_baseUrl$endpoint');
+    print('✅ Response Data (Review): ${response.data}');
+    return response.data;
+  }
+
+  @override
+  Future<Map<String, dynamic>> post({required String endpoint, required Map<String, dynamic> body}) async{
+    var response = await _dio.post('$_baseUrl$endpoint', data: body);
     print('✅ Response Data (Review): ${response.data}');
     return response.data;
   }
