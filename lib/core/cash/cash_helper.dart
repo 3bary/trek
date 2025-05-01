f5d1bd60b3c48bbdce00b312a07305f7ff7e6612
Mohamed Ashraf <medoalex350@gmail.com>
2025-05-01 12:56:39 +0300

    handle potential null _prefs in cash helper

--- lib/core/cash/cash_helper.dart
+++ lib/core/cash/cash_helper.dart
@@ -11,18 +11,23 @@ class CashHelper {
 
   static Future<void> cacheUser(UserModel user) async {
     final userJson = jsonEncode(user.toJson());
-    await _prefs!.setString('CACHED_USER', userJson);
-  }
+    if (_prefs != null) {
+      await _prefs!.setString('CACHED_USER', userJson);
+    }  }
 
   static UserModel? getCachedUser() {
-    final userJson = _prefs!.getString('CACHED_USER');
-    if (userJson != null) {
-      return UserModel.fromJson(jsonDecode(userJson));
+    if (_prefs != null) {
+      final userJson = _prefs!.getString('CACHED_USER');
+      if (userJson != null) {
+        return UserModel.fromJson(jsonDecode(userJson));
+      }
     }
     return null;
   }
 
   static Future<void> clearCachedUser() async {
-    await _prefs!.remove('CACHED_USER');
+    if (_prefs != null) {
+      await _prefs!.remove('CACHED_USER');
+    }
   }
 }
