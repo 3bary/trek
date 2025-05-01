2cfce93fae71ad768ba61e229047462a62f767b7
Mohamed Ashraf <medoalex350@gmail.com>
2025-05-01 07:21:30 +0300

    Implement DioClient and edit service locator

--- lib/core/utils/service_locator.dart
+++ lib/core/utils/service_locator.dart
@@ -1,24 +1,18 @@
 
-import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 import 'package:greendo/core/network/recommendation_api_service.dart';
-
 import '../../features/home/data/repos/home/home_repo_imp.dart';
+import '../network/dio_client.dart';
 
 final getIt = GetIt.instance;
 
 void setupServiceLocator() {
+  final dio = DioClient.instance;
   getIt.registerLazySingleton<HomeRepoImp>(
     () => HomeRepoImp(
       RecommendationApiService(
-        Dio(options),
+        dio,
       ),
     ),
   );
 }
-
-BaseOptions options = BaseOptions(
-  receiveDataWhenStatusError: true,
-  receiveTimeout: const Duration(seconds: 60),
-  connectTimeout: const Duration(seconds: 30),
-);
